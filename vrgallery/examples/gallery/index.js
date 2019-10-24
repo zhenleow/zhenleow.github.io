53a4ea09b7d66c30e9adb9d4f90411895e90acc7
Zhen Zhen LEOW <leow_zhen_zhen@nyp.edu.sg>
2019-10-24 23:14:12 +0800

    Update index.js

--- vrgallery/examples/gallery/index.js
+++ vrgallery/examples/gallery/index.js
@@ -41,6 +41,14 @@ var scenes = {
   },
 };
 
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  wh = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--wh', `${wh}px`);
+});
+
 function onLoad() {
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 vh = window.innerHeight * 0.01;
