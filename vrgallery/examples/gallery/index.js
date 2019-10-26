4416e32c928417f03f2683f622e60cfb75ef0b66
Zhen Zhen LEOW <leow_zhen_zhen@nyp.edu.sg>
2019-10-26 21:55:16 +0800

    Update index.js

--- vrgallery/examples/gallery/index.js
+++ vrgallery/examples/gallery/index.js
@@ -41,16 +41,16 @@ var scenes = {
   },
 };
 
-window.addEventListener('resize', () => {
+//window.addEventListener('resize', () => {
   // We execute the same script as before
-  onLoad();
+  //onLoad();
   /*vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
   wh = window.innerWidth * 0.01;
   document.documentElement.style.setProperty('--wh', `${wh}px`);
   viewport = document.querySelector("meta[name=viewport]");
   viewport.setAttribute('content', 'width='+wh+', height='+vh+', initial-scale=1.0');*/
-});
+//});
 
 window.onorientationchange=function()
 {
