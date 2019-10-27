84ec3ac36ce71766eb83746fda5796b06cc82ee6
Zhen Zhen LEOW <leow_zhen_zhen@nyp.edu.sg>
2019-10-27 20:19:05 +0800

    b

--- vrgallery/examples/gallery2/index.js
+++ vrgallery/examples/gallery2/index.js
@@ -33,8 +33,8 @@ var scenes = {
     preview: 'chichen-itza-preview.jpg'
   },
   tajMahal: {
-    image: 'taj-mahal.jpg',
-    preview: 'taj-mahal-preview.jpg'
+	image: 'hotel-lobby.jpg',
+    preview: 'hotel-lobby.jpg'
   },
 };
 
